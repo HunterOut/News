3c9f0784305394917d7f3862604f7ad47fff6004
hunter_out <hunter_out@icloud.com>
2020-06-09 15:39:21 +0300

    изменил путь

--- news_list/urls.py
+++ news_list/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'news'
+app_name = 'news_list'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
