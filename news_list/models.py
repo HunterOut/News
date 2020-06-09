c623728515050e179bb22745f25bba581549a588
hunter_out <hunter_out@icloud.com>
2020-06-09 15:40:13 +0300

    добавил паблиш-менеджер, изменил модель новостей

--- news_list/models.py
+++ news_list/models.py
@@ -3,12 +3,32 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField(max_length=1500, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='news_posts')
     publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+    class Meta:
+        ordering = ('-publish',)
 
     def __str__(self):
         return self.title
