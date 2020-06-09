5537561f30260692cb5d60dff3d627e284b86486
hunter_out <hunter_out@icloud.com>
2020-06-09 19:14:22 +0300

    добавил абсолют_урл

--- news_list/models.py
+++ news_list/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -26,6 +27,13 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('news_list:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
     class Meta:
         ordering = ('-publish',)
